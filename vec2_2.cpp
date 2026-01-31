3fe8a29c56d1cff9f58f31cedc5f3b0ea21bf59e
Pronus <2055967434@qq.com>
2026-01-31 16:08:29 +0800

    vec2_2 passed

--- vec2_2.cpp
+++ vec2_2.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
 using namespace std;
 
+class Solution {
+public:
+    static vector<int> findDisappearedNumbers(vector<int>& nums) {
+        const size_t n = nums.size();
+        vector<int> result;
+        for (size_t i = 0; i < n; ++i) {
+            if (const int idx = abs(nums[i]) - 1; nums[idx] > 0) {
+                nums[idx] = -nums[idx];
+            }
+        }
+
+        // 第二次遍历：找出未被标记的位置
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                result.push_back(i + 1);
+            }
+        }
+        return result;
+    }
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    int num;
+    vector<int> nums;
+    while (cin>>num) {
+        nums.push_back(num);
+    }
+
+    for (const auto& i : Solution::findDisappearedNumbers(nums)) {
+        cout << i << endl;
+    }
+
+    return 0;
 }
